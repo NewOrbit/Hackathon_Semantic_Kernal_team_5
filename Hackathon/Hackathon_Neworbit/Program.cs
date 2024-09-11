fd05f16ac5c417b45adc3d19eba8c86a2d5397c1
Jakub Koleżyński <1st.cubix@gmail.com>
2024-09-11 13:20:27 +0200

    fork init

--- Hackathon/Hackathon_Neworbit/Program.cs
+++ Hackathon/Hackathon_Neworbit/Program.cs
@@ -2,55 +2,56 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Embeddings;
+
+#pragma warning disable SKEXP0010
+#pragma warning disable SKEXP0001
 
 var builder = Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings()
 {
     Args = args
-} );
+});
 
 // don't forget to add your api key / endpoint / deployment name/and model id ( deployments found here: https://oai.azure.com/ )
 builder.Services
-    //.AddAzureOpenAIAudioToText()
-    //.AddAzureOpenAITextEmbeddingGeneration()
-    //.AddAzureOpenAITextToImage()
     .AddAzureOpenAIChatCompletion(
-        "",
-        "",
-        "",
+        "hack-2024-gpt-4o",
+        "https://hack-2024-oai.openai.azure.com/",
+        "4705922971104f64bab77a51cb68b37d",
         modelId: "gpt-4o");
-        
+
+// How to do embeddings
+builder.Services
+    .AddAzureOpenAITextEmbeddingGeneration(
+        "hack-2024-embeddings",
+        "https://hack-2024-oai.openai.azure.com/",
+        "4705922971104f64bab77a51cb68b37d",
+        modelId: "gpt-4o");
+
 var app = builder.Build();
 
 var chat = app.Services.GetRequiredService<IChatCompletionService>();
-
 var chatHistory = new ChatHistory();
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: What am I?");
-Console.ForegroundColor = ConsoleColor.Yellow;
+// create embeddings
+var embeddings = app.Services.GetRequiredService<ITextEmbeddingGenerationService>();
 
-Console.WriteLine("Jay: ");
-var whatAmI = Console.ReadLine();
-chatHistory.AddSystemMessage(whatAmI!);
+chatHistory.AddSystemMessage(
+    "You are a pharmacist trying to help choose the right medication for a patient. The patient");
 
 Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: Cool! How can I help?");
-Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Chat GP: Hello!. What ails you?");
 
-Console.WriteLine("Jay: ");
-var prompt = Console.ReadLine();
-chatHistory.AddUserMessage(prompt!);
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Jay: ");
+    var prompt = Console.ReadLine();
+    chatHistory.AddUserMessage(prompt!);
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: ");
-var response = await chat.GetChatMessageContentsAsync(chatHistory);
-var lastMessage = response.Last();
-Console.WriteLine(lastMessage);
-
-// You can create a kernal and pass it to `GetChatMessageContentsAsync`, you can add plugins to this kernal to control the behavior of the AI 
-// https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-csharp
-
-// detailed samples
-// https://learn.microsoft.com/en-us/semantic-kernel/get-started/detailed-samples?pivots=programming-language-csharp
-
-
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("AI: ");
+    var response = await chat.GetChatMessageContentsAsync(chatHistory);
+    var lastMessage = response.Last();
+    Console.WriteLine(lastMessage);
+}
